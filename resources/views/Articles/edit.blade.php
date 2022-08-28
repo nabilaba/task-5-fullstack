fc77de24af16c34fa2ea9da83988454faf79fb04
nabilaba <nabilazizbimaanggita@gmail.com>
2022-08-28 17:52:53 +0700

    upload task

---
+++ resources/views/Articles/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="continer">
+    <div class="row justify-content-center">
+        <div class="card mx-5 col-6 ">
+            <div class="card-body ">
+                <h4 class="card-header">Edit Article</h4>
+                <form action="/articles/{{ $article->id }}" method="POST">
+                @method('PUT')
+                    @csrf
+                    <div class="card-text pt-3">					
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $article->title) }}" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Content</label>
+                            <textarea class="form-control" name="content" id="content" required>{{ old('content', $article->content) }}</textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Image Url</label>
+                            <input type="text" class="form-control" name="image" id="image" value="{{ old('image', $article->image) }}">
+                        </div>
+                        <div class="form-group">
+                            <label>Category Name</label>
+                            <select class="form-select" id="category_id" aria-label="Default select example" name="category_id" value="{{ old('category_id', $article->category_id) }}">
+                                <option selected>Choose</option>
+                                @foreach ($categories as $category)
+                                @if(old('category_id', $article->category_id) == $category->id)
+                                    <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+                                @else 
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endif
+                                @endforeach
+                            </select>
+                        </div>					
+                    </div>
+                    <hr>
+                    <div class="mt-4 d-flex justify-content-end">
+                        <a href="/articles" class="btn btn-default mx-3" >Cancel</a>
+                        <input type="submit" class="btn btn-success" value="Edit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container my-5 py-5">
+        <div class="text-center">
+            <h1>404 | error not found</h1>
+        </div>
+    </div>
+@endsection
